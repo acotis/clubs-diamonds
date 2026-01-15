b419515561876fdfd5f56dfb5282529435dd0a9a
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2026-01-15 06:40:59 -0800

    Made constants go up to 155.

--- src/search/writer/writers/const_writer.rs
+++ src/search/writer/writers/const_writer.rs
@@ -5,26 +5,55 @@ use super::super::*;
 pub struct ConstWriter {
     length: usize,
     next_write: u8,
+    remove_neg_at: u8,
+    stop_at: u8
 }
 
 impl ConstWriter {
     pub fn new(length: usize) -> Self {
-        Self {
-            length,
-            next_write: 1,
+        match length {
+            1 => {
+                Self {
+                    length,
+                    next_write: 1, // todo: should this be 0?
+                    remove_neg_at: 1,
+                    stop_at: 10,
+                }
+            },
+            2 => {
+                Self {
+                    length,
+                    next_write: 1,
+                    remove_neg_at: 10,
+                    stop_at: 100,
+                }
+            }
+            3 => {
+                Self {
+                    length,
+                    next_write: 10,
+                    remove_neg_at: 100,
+                    stop_at: 156,
+                }
+            }
+            _ => panic!(),
         }
     }
 
     pub fn write(&mut self, dest: &mut [u8]) -> bool {
-        if self.next_write > 9 {
+        if self.next_write == self.stop_at {
             return false;
         }
 
-        if self.length == 2 {
-            dest[1] = OpPivot(Op::NOT).encode();
+        dest[..self.length].fill(Nop.encode());
+
+        if self.next_write >= self.remove_neg_at {
+            dest[self.length-1] = ConstPivot(self.next_write).encode();
+        } else {
+            dest[self.length-2] = ConstPivot(self.next_write).encode();
+            dest[self.length-1] = OpPivot(Op::NOT).encode();
         }
 
-        dest[0] = ConstPivot(self.next_write).encode();
         self.next_write += 1;
         true
     }
