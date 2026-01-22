8f880ab285ec160323419cf947f4aa3270bbfa0d
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2026-01-21 19:58:23 -0800

    Created unvar(), and tests for revar() and unvar().

--- src/search/expression/revar.rs
+++ src/search/expression/revar.rs
@@ -1,6 +1,7 @@
 
 pub trait Revar {
     fn revar(self, _: &[char]) -> String;
+    fn unvar(self, _: &[char]) -> String;
 }
 
 impl Revar for &str {
@@ -12,5 +13,20 @@ impl Revar for &str {
             })
             .collect()
     }
+
+    fn unvar(self, old_names: &[char]) -> String {
+        self.chars()
+            .map(|c| match old_names.iter().position(|x| *x == c) {
+                Some(index) => "abcdefghijklmnopqrstuvwxyz".chars().nth(index).unwrap(),
+                None => c,
+            })
+            .collect()
+    }
+}
+
+#[test]
+fn unvar_revar() {
+    assert_eq!("(i+j)/k*j", "(b+a)/c*a".revar(&['j', 'i', 'k']));
+    assert_eq!("(b+a)/c*a", "(i+j)/k*j".unvar(&['j', 'i', 'k']));
 }
 
